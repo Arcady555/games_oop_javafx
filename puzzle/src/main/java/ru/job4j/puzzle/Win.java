017f8b67c273893a642161e917fe845f9a56df89
Arrcady555 <parfenov7233@gmail.com>
2021-11-17 13:51:39 +0300

    2. Сокобан. Тестовое задание. [#75283]- 2

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,39 +3,33 @@ package ru.job4j.puzzle;
 public class Win {
 
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        if (!monoHorizontal(board) && !monoVertical(board)) {
-            rsl = false;
+        boolean rsl = false;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 1 && (monoHorizontal(board, index) || monoVertical(board, index))) {
+                rsl = true;
+                break;
+            }
         }
         return rsl;
     }
-    public static boolean monoHorizontal(int[][] board) {
+
+    public static boolean monoHorizontal(int[][] board, int row) {
         boolean result = true;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (board[i][j] != 1) {
-                    result = false;
-                    break;
-                }
-                if (board[i][board.length - 1] == 1) {
-                    return true;
-                }
+        for (int index = 0; index < board[row].length; index++) {
+            if (board[row][index] != 1) {
+                result = false;
+                break;
             }
         }
         return result;
     }
 
-    public static boolean monoVertical(int[][] board) {
+    public static boolean monoVertical(int[][] board, int column) {
         boolean result = true;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (board[j][i] != 1) {
-                    result = false;
-                    break;
-                }
-                if (board[board.length - 1][j] == 1) {
-                    return true;
-                }
+        for (int[] ints : board) {
+            if (ints[column] != 1) {
+                result = false;
+                break;
             }
         }
         return result;
